5678970f512908c0a96a20ce4acd16f7733abda0
Kim <Khangnyon.Kim@yokogawa.com>
2025-07-21 14:45:00 +0900

    trimmed unused folders

--- team_report_app/delete_db.py
+++
@@ -1,9 +0,0 @@
-import os
-
-db_path = 'instance/updates.db'
-
-if os.path.exists(db_path):
-    os.remove(db_path)
-    print(f"Deleted database file at {db_path}")
-else:
-    print(f"No database file found at {db_path}")
